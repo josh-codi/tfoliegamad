19d699b39f7f13c24152fe810053595cb98cbfca
Odame Joshua <73438906+josh-codi@users.noreply.github.com>
2024-10-17 20:42:42 +0000

    Update tfoliegamad.js

--- tfoliegamad.js
+++ tfoliegamad.js
@@ -1,5 +1,5 @@
 function shouldtfoliegamad() {
-  return false; // Update this as needed
+  return true; // Update this as needed
 }
 // Expose it globally to be accessed from anywhere
 window.shouldtfoliegamad = shouldtfoliegamad;
